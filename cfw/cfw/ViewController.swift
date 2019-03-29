ffabe5e429457c03d678a6727ff8b7899d28de98
Johannes Neutze <johannes@neutze.de>
2019-03-29 12:25:50 +0100

    Setup workspace (#1)

---
+++ cfw/cfw/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  cfw
+//
+//  Created by Johannes Neutze on 29.03.19.
+//  Copyright © 2019 Johannes Neutze. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
